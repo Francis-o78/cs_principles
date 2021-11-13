51f65dd4cf8be5c01779f89e56288ff4674a20f3
Francis <francis.ocapt78@gmail.com>
2021-11-12 20:59:10 -0800

    Game JS Update #11
    new changes made to game JS file; still working on a glitch when colliding with the red obstacle bars
    kept Mr. Cozort's comments & commented code for reference.

--- js/game.js
+++ js/game.js
@@ -1,14 +1,21 @@
-// global variables are defined at top here
+// global variables go up at the top
+// defines width and height variables, sets up structures for use throughout
 let canvas;
 let ctx;
-let WIDTH = 600;
-let HEIGHT = 400;
 
-// get user input from keyboard; establishing ideally keystrokes 's' and 'w'
+let TILESIZE = 64;
+let WIDTH = TILESIZE * 22;
+let HEIGHT = TILESIZE * 9;
+let allSprites = [];
+let walls = [];
+// defined various variables (walls, Sprites, etc.) for game grid
+
+
+
+// user input from keyboard to control box
 let keysDown = {};
 let keysUp = {};
 
-// defines area where boxes are traveling, basically the arena of the game
 let gamePlan = `
 ......................
 ..#................#..
@@ -19,55 +26,26 @@ let gamePlan = `
 ......#............#..
 ......##############..
 ......................`;
+// defines game grid bars
 
 
-
-function buildLevel(plan, width) {
-    // function that makes the grid for which boxes to travel on
-    // setup arrays to hold information from gameplan
-    let newRow = [];
-    let newGrid = [];
-
-    // for loop that defines conditions for moving boxes based on input
-    // also specifies that if box is moved to new row on grid and 
-    // returns new length of the array once moved
-
-    for (i of plan) {
-
-        if (i != "\n") {
-            newRow.push(i);
-        }
-
-        if (newRow.length % width == 0 && newRow.length != 0) {
-            console.log(newRow.length);
-            newGrid.push(newRow);
-            newRow = [];
-            console.log(newGrid);
-        }
-    }
-    console.log(newRow);
-    return newGrid;
-}
-//above if statements print the variables defined - newgrid, newRow, etc. to establish movement of box (== 0)
 
 addEventListener("keydown", function (event) {
     keysDown[event.key] = true;
-    console.log("key down is " + keysDown[event.key]);
-    console.log(keysDown);
+    // console.log("key down is " + keysDown[event.key]);
 }, false);
-//basically specifies the type of event to pay attention to (that being the movement of boxes downward) and defines what happens when the box moves in this direction
+// establishes function for moving box down and what happens when that's done
 
-//basically specifies the type of event to pay attention to (that being the movement of boxes upward) and defines what happens when the box moves in this direction
 addEventListener("keyup", function (event) {
     // keysUp[event.key] = true;
     delete keysDown[event.key];
-    console.log(keysDown);
-    console.log(keysUp);
 }, false);
+// establishes function for moving box up and what happens when that's done
 
-// initialization function = setup canvas and context
-// function function referenced at HTML document in body onload = ""
-// append specific content: used to put specific content within specified elements.
+
+// here we use init (short for initialize) to setup the canvas and context
+// this function will be called in the HTML document in body onload = ""
+// we also append the body with a new canvas element
 function init() {
     canvas = document.createElement("canvas");
     canvas.width = WIDTH;
@@ -77,45 +55,35 @@ function init() {
     document.body.appendChild(canvas);
     gameLoop();
 }
-// defines width height variables and loops game to allow for constant movement; also gives context to object at play in init function ()
-//get Context defines a returnable-object that specific characteristics of width, color, height, etc.
+// function initialization to paint canvas and various widths/heights for the red bars
 
-
-class Square {
-    constructor(id, x, y, speed, w, h, color) {
-        this.id = id;
+class Sprite {
+    constructor(x, y, w, h, color) {
         this.x = x;
         this.y = y;
-        this.speed = speed;
         this.w = w;
         this.h = h;
         this.color = color;
+        allSprites.push(this);
     }
-//defines parameters for squares on page; specific variables like movement, color, height and width
-    
-    create(x, y, speed, w, h, color) {
-        return new Square(x, y, speed, w, h, color);
+    get type() {
+        return "sprite";
     }
-    update() {
-        if (this.x >= WIDTH - this.w || this.x < 0) {
-            // console.log("I fell off the side!!!!")
-            this.speed = -this.speed;
-        }
-        this.x += this.speed;
-        // this.y += Math.random()*5*this.speed;
-        // console.log(this.x);
-    };
+    create(x, y, w, h, color) {
+        return new Sprite(x, y, w, h, color);
+    }
     draw() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     };
 }
-//update takes the input and changes the hash of the object at hand - the update function returns the index of the input that is given
+// defines specifics behind variables and functions contained in Sprite class. defines width, height, and other variables.
+// create and draw operations draw actor Sprite with specified characteristics
 
-class Player extends Square {
-    constructor(id, x, y, speed, w, h, color, hitpoints) {
-        super(id, x, y, speed, w, h, color);
-        this.id = id;
+
+class Player extends Sprite {
+    constructor(x, y, speed, w, h, color, hitpoints) {
+        super(x, y, w, h, color);
         this.x = x;
         this.y = y;
         this.speed = speed;
@@ -124,100 +92,231 @@ class Player extends Square {
         this.color = color;
         this.hitpoints = hitpoints;
         // console.log(this.hitpoints);
-        // defines Player class, allowing different inputs and characteristics of Square class characters.
     }
-    update() {
+    // this class extends the characteristics and variables defined in the Sprite class - refers to the character you are controlling
+
+    collideWith(obj) {
+        if (this.x + this.w > obj.x &&
+            this.x < obj.x + obj.w &&
+            this.y + this.h > obj.y &&
+            this.y < obj.y + obj.h
+        ) {
+            console.log(this.type + ' collides with ' + obj.type);
+            return true;
+        }
+    }
+    get type() {
+        return "player";
+    }
+    // collision operation basically pointing out what happens when character Sprite is put in contact with red bars
+    input() {
         if ('w' in keysDown) {
+            this.dy = -1;
+            this.dx = 0;
+            // console.log("dy is: " + this.dy)
             this.y -= this.speed;
         }
         if ('a' in keysDown) {
+            this.dx = -1;
+            this.dy = 0;
+            // console.log("dx is: " + this.dx)
             this.x -= this.speed;
         }
         if ('s' in keysDown) {
+            this.dy = 1
+            this.dx = 0;
+            // console.log("dy is: " + this.dy)
             this.y += this.speed;
+
         }
         if ('d' in keysDown) {
+            this.dx = 1;
+            this.dy = 0;
+            // console.log("dx is: " + this.dx)
             this.x += this.speed;
         }
-        //defines key characters for controlling square
+        // input function defines what happens when inputting into keyboard each letter
 
+    }
+    update() {
+        this.input();
         // this.y += Math.random()*5*this.speed;
         // console.log(this.x);
+        if (this.x + this.w > WIDTH) {
+            this.x = WIDTH - this.w;
+        }
+        if (this.x <= 0) {
+            this.x = 0;
+        }
+        if (this.y + this.h > HEIGHT) {
+            this.y = HEIGHT - this.h;
+        }
+        if (this.y <= 0) {
+            this.y = 0;
+        }
     };
 }
+// update refers to the if statements stating what conditions execute based on where the Sprite box moves. Depending on the Sprite's movement across the axes, 
+// HEIGHT or width gets affected
+
+class Enemy extends Player {
+    constructor(x, y, speed, w, h, color, hitpoints) {
+        super(x, y, speed, w, h, color, hitpoints);
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.hitpoints = hitpoints;
+        // console.log(this.hitpoints);
+    }
+    // Enemy class still in the works but its extending same characteristics and variables in Player class
+
+    get type() {
+        return "enemy";
+    }
+
+}
+
+let badguy = new Enemy();
+console.log("here's the example of a sub-sub class " + badguy.type);
+console.log("badguy stats " + badguy.speed);
+// connects Enemy class just extended from player class to define badguy - defines speed, type of up and coming enemy.
+
+class Wall extends Sprite {
+    constructor(x, y, w, h, color) {
+        super(x, y, w, h, color);
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+    }
+    create(x, y, w, h, color) {
+        return new Wall(x, y, w, h, color);
+    }
+    get type() {
+        return "wall";
+    }
+}
+// extension of Sprite class basically defines characteristics of obstacles (in red) that the sprite needs to move around
+
 
 const levelChars = {
     ".": "empty",
-    "#": Square,
+    "#": Wall,
 };
 
-// let level1 = buildLevel(gamePlan, 22);
-
-function readLevel() {
-    /* 
-    Create array to hold starting actors or elements on the screen
-    */
-    let startActors = [];
-    /* read game plan - the 'i' below will be the 'y' value */
-    for (i of gamePlan) {
-        console.log(i + " will be the y value...");
-        /* checks to see if we hit a newline;
-        if not, set variable 'type' to the current level character
-        in the object we created called levelChars
-        */
+function makeGrid(plan, width) {
+    let newGrid = [];
+    let newRow = [];
+    for (i of plan) {
         if (i != "\n") {
-            let type = levelChars[i];
-            /* if level type is a 'string', not a class
-            push it into start actors as is
+            newRow.push(i);
+        }
+        if (newRow.length % width == 0 && newRow.length != 0) {
+            newGrid.push(newRow);
+            newRow = [];
+        }
+    }
+    return newGrid;
+}
+// function that makes the red obstacles the character needs to move around; if statements to determine conditions on which ox moves to different rows/columns on made 
+// grid
+
+console.log("here's the grid...\n" + makeGrid(gamePlan, 22));
+
+function readLevel(grid) {
+    let startActors = [];
+    // note the change from i to x and y
+    for (y in grid) {
+        for (x in grid[y]) {
+            /*              crate a variable based on the current
+            item in the two dimensional array being read
+             */
+            let ch = grid[y][x];
+            /* if the character is not a new line character
+            create a variable from the value attached to the 
+            key in the object, e.g. 
+
+            const levelChars = {
+                ".": "empty",
+                "#": Square,
+            };
+
+            where "." is the key and the value is "empty"
+            In the case of "#", the key is "#" and the value
+            is the Square class.
             
             */
-            if (typeof type == "string") {
-                startActors.push(type);
-                console.log('string found');
-            }
-            /* if it is not a string, it will be a class as we've written it; if it is a class, push a newly created object into the startActors, 
-            with all necessary arguments or values.
-            In this case, x,y,w,h,color
-            
-            */
-            else {
-                i = new type;
-                startActors.push(i.create(10, 20, 1, 30, 40, 'red'))
+            if (ch != "\n") {
+                let type = levelChars[ch];
+                if (typeof type == "string") {
+                    startActors.push(type);
+                } else {
+                    let t = new type;
+                    // let id = Math.floor(100*Math.random());
+                    /*  Here we can use the x and y values from reading the grid, 
+                        then adjust them based on the tilesize
+                         */
+                    startActors.push(t.create(x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE, 'red'))
+                }
             }
         }
     }
-    console.log(startActors);
-    // for loops and if statements  defining specific conditions for current level of game; 
+    return startActors;
 }
 
-readLevel()
+
+let currentLevel = readLevel(makeGrid(gamePlan, 22))
+console.log('current level');
+console.log(currentLevel);
 
 // instantiations...
-let player1 = new Player("Me", WIDTH / 2, HEIGHT / 2, 1, 40, 40, 'rgb(300, 50, 100)', 100);
-let oneSquare = new Square("Harry", 10, 10, 1, 50, 50, 'rgb(400, 60, 200)');
-let twoSquare = new Square("yeezus", 60, 60, 5, 100, 100, 'rgb(200, 200, 0)');
-let threeSquare = new Square("boppa", 70, 70, 3, 25, 25, 'rgb(100, 100, 300)');
+let player1 = new Player(WIDTH / 2, HEIGHT / 2, 10, TILESIZE, TILESIZE, 'rgb(100, 100, 100)', 100);
+// let oneSquare = new Square("Bob", 10, 10, 1, 50, 50, 'rgb(200, 100, 200)');
+// let twoSquare = new Square("Chuck", 60, 60, 5, 100, 100, 'rgb(200, 200, 0)');
+// let threeSquare = new Square("Bill", 70, 70, 3, 25, 25, 'rgb(100, 100, 222)');
 
-let allSprites = [oneSquare, twoSquare, threeSquare, player1];
-//specifies various actors in game, different squares and assigning various values and locations to them
-
+console.log(allSprites);
+console.log(walls);
 
 function update() {
     for (i of allSprites) {
-        // console.log(i);
-        i.update();
+        if (i.type == "wall") {
+            // console.log(i)
+            if (player1.collideWith(i)) {
+                if (player1.dx == 1){
+                    player1.x = i.x - player1.w;
+                }
+                else if (player1.dx == -1){
+                    player1.x = i.x + i.w;
+                }
+                else if (player1.dy == 1){
+                    player1.y = i.y - player1.h;
+                }
+                else if (player1.dy == -1){
+                    player1.y = i.y + i.h;
+                }
+                // console.log("player collided with walls")
+                console.log("player1 dx is:" + player1.dx);
+            }
+        }
     }
+    // update function outlining if/else statement-conditions that say that based on where the player moves, the function substracts/adds the height or width
+
+    player1.update();
 
     // oneSquare.update();
     // twoSquare.update();
 }
-// draw function gives context for rectangle dimensions and characteristics
+// we now have just the drawing commands in the function draw
 function draw() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     for (i of allSprites) {
         // console.log(i);
         i.draw();
-        // i draw...?
     }
 }
 // here we have a big leap!
@@ -234,5 +333,5 @@ let gameLoop = function () {
     update();
     draw();
     window.requestAnimationFrame(gameLoop);
-    // loops game to allow continued play; asks browser to redraw image after input is given (requestAnimationFramez)
+    // basically tells html chrome to drawn overall canvas and tells the browser to redraw the canvas based on what happens and the input given
 }
